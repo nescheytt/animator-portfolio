6f48e34c98c83cc48174a0fb6516d5a1621ce01a
nescheytt <nahuel.scheytt@gmail.com>
2023-03-05 19:45:03 +0000

    feat: home create component social network

---
+++ src/pages/home/components/social-media/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Component = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const LinkSocial = styled.a`
+  margin: 0px 0px 0px 10px;
+`
