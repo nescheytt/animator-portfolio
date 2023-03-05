6c0070aa18c129930669c5112fa02256cee37f78
nescheytt <nahuel.scheytt@gmail.com>
2023-03-05 19:45:43 +0000

    feat: home create component meta

---
+++ src/pages/home/components/meta/index.tsx
@@ -0,0 +1,11 @@
+import { Helmet } from "react-helmet";
+
+export const Meta = () => {
+  return (
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Facundo López - Animator</title>
+      <meta name="description" content="Hi! I'm Facundo López. Argentinian animator based in Buenos Aires. I love animation and music everyday, every minute, every second." />
+    </Helmet>
+  )
+};
