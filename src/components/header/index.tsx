8d66f4cde8bbc1cbcd6eb92786cfdf0837fbf633
nescheytt <nahuel.scheytt@gmail.com>
2023-03-09 16:48:57 +0000

    feat: add anchor bar into header + header show

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,33 +1,100 @@
-import { Row, Col } from 'antd'
-import { Component, AnchorCustom } from './styles'
+import { useEffect, useState } from 'react';
+import { Row, Col, Grid } from 'antd';
+import { Component, BurgerContainer, Burger, Screen, AnchorScreenNavbar, Sticky } from './styles';
+import { AnchorBar } from '../anchor-bar';
+
+const NavItems = [
+  {
+    key: 'work',
+    href: '#work',
+    title: 'Work',
+  },
+  {
+    key: 'about',
+    href: '#about',
+    title: 'About',
+  },
+  {
+    key: 'contact',
+    href: '#contact',
+    title: 'Contact',
+  }
+];
+
+const links = [
+  { label: 'Work', href: '#work' },
+  { label: 'About', href: '#about' },
+  { label: 'Contact', href: '#contact' },
+];
 
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [renderScreenMenu, setRenderScreenMenu] = useState(false);
+  const [visible, setVisible] = useState(false);
+
+  const { useBreakpoint } = Grid;
+  const { md } = useBreakpoint();
+
+  const handlerOnClickBurger = () => {
+    setIsOpen(!isOpen);
+    setRenderScreenMenu(!isOpen);
+  };
+
+  let timeout: NodeJS.Timeout;
+
+  useEffect(() => {
+    setVisible(true);
+
+    const handleScroll = () => {
+      if (window.pageYOffset === 0) {
+        console.log('pasa por aca');
+        setVisible(true);
+        clearTimeout(timeout);
+      } else {
+        setVisible(true);
+        clearTimeout(timeout);
+
+        timeout = setTimeout(() => {
+          setVisible(false);
+        }, 1000);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      clearTimeout(timeout);
+    };
+  }, []);
+
   return (
     <Component>
       <Row>
-        <Col span={24} >
-          <AnchorCustom
-            direction="horizontal"
-            items={[
-              {
-                key: 'work',
-                href: '#work',
-                title: 'Work',
-              },
-              {
-                key: 'about',
-                href: '#about',
-                title: 'About',
-              },
-              {
-                key: 'contact',
-                href: '#contact',
-                title: 'Contact',
-              }
-            ]}
-          />
+        <Col span={24}>
+          {md ? (
+            <Sticky scrolled={visible}>
+              <AnchorBar direction="horizontal" size="large" links={links} scrollDelay={1000} />
+            </Sticky>
+          ) : (
+            <>
+              <BurgerContainer>
+                <Burger onClick={handlerOnClickBurger} isOpen={isOpen}>
+                  <span className="line-1" />
+                  <span className="line-2" />
+                  <span className="line-3" />
+                </Burger>
+              </BurgerContainer>
+
+              {renderScreenMenu && (
+                <Screen isOpen={isOpen}>
+                  <AnchorScreenNavbar direction="vertical" onClick={handlerOnClickBurger} items={NavItems} />
+                </Screen>
+              )}
+            </>
+          )}
         </Col>
       </Row>
     </Component>
-  )
-}
+  );
+};
--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -1,55 +1,101 @@
-import { Anchor } from 'antd'
-import styled from 'styled-components'
+import { Anchor } from 'antd';
+import styled from 'styled-components';
 
 export const Component = styled.header`
-  margin-top: -120px;
+  padding: 20px;
   position: sticky;
   top: 0px;
   z-index: 1;
-`
+`;
 
-export const AnchorCustom = styled(Anchor)`
-  height: 120px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+export const Sticky = styled.div<{ scrolled?: boolean }>`
+  transition: opacity 0.3s ease-in-out;
+  opacity: 0;
 
-  /**
-   * Styles forced anchorbar
-   */
-  &:before {
-    border-bottom: none;
+  &:hover {
+    opacity: 1;
   }
 
-  & > div {
-    &:before {
-      border-inline-start: none !important;
-    }
-  }
+  ${({ scrolled }) => scrolled && `
+    opacity: 1;
+  `}
+`;
 
+export const AnchorScreenNavbar = styled(Anchor)`
   a {
     font-size: 32px;
   }
-`
+`;
 
-export const List = styled.ul`
-  height: 120px;
-  list-style: none;
-  margin: 0px;
-  padding: 0px;
+export const BurgerContainer = styled.div`
+  height: 90px;
+  padding: 15px;
   display: flex;
-  justify-content: center;
   align-items: center;
-`
+  justify-content: right;
+`;
 
-export const Item = styled.li`
-  margin-right: 24px;
+const line1 = `
+  border-top: 5px solid #000;
+  border-radius: 10px;
+  display: block;
+  transition: opacity 0.3s, margin 0.5s, transform 0.5s;
+  width: 50px;
+`;
 
-  &:last-child {
-    margin-right: 0px;
-  }
-`
+export const Burger = styled.div<{ isOpen: boolean }>`
+  transition: transform 0.5s;
+  z-index: 99;
+	
+	.line-1 {
+		${line1};
+	}
+	
+	.line-2 {
+		${line1};
+		margin: 10px 0;
+	}
+	
+	.line-3 {
+		${line1};
+	}
 
-export const Link = styled.a`
-  font-size: 32px;
-`
+  ${({ isOpen }) => isOpen && `
+    padding: 25px 0;
+		
+		.line-1 {
+      ${line1};
+			transform: rotate(45deg);
+		}
+
+		.line-2 {
+      ${line1};
+			margin: -5px 0;
+			opacity: 0;
+		}
+
+		.line-3 {
+      ${line1};
+			transform: rotate(-45deg);
+		}
+  `}
+`;
+
+export const Screen = styled.div<{ isOpen: boolean }>`
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(255, 215, 206, 0.9);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  position: absolute;
+  transition: opacity 4s;
+  opacity: 0;
+
+  ${({ isOpen }) => isOpen && `
+    opacity: 1;
+  `}
+`;
