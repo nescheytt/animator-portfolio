a9b9585a1d35d239ebd8ae055fea224ccc0cb8a3
nescheytt <nahuel.scheytt@gmail.com>
2023-03-10 15:53:43 +0000

    feat: add items work with box-hover

--- src/pages/home/components/work/index.tsx
+++ src/pages/home/components/work/index.tsx
@@ -1,46 +1,92 @@
+import { useState } from 'react';
 import { Row, Col } from 'antd';
 
 // styles
-import { Component } from './styles';
+import { Component, WrapperImage, Hovered, Image, Title, LinkWork } from './styles';
 
 // images
 import bkg1 from '../../../../assets/background-1.jpg';
 import bkg2 from '../../../../assets/background-2.gif';
 import bkg3 from '../../../../assets/background-3.gif';
 
+const listWorks = [
+  {
+    title: 'Title Project',
+    label: 'See project description',
+    url: '/',
+    image: bkg1,
+    alt: 'work-1',
+    col: 24,
+  },
+  {
+    title: 'Title Project',
+    label: 'See project description',
+    url: '/',
+    image: bkg2,
+    alt: 'work-2',
+    col: 12,
+  },
+  {
+    title: 'Title Project',
+    label: 'See project description',
+    url: '/',
+    image: bkg3,
+    alt: 'work-3',
+    col: 12,
+  },
+  {
+    title: 'Title Project',
+    label: 'See project description',
+    url: '/',
+    image: bkg1,
+    alt: 'work-4',
+    col: 24,
+  },
+  {
+    title: 'Title Project',
+    label: 'See project description',
+    url: '/',
+    image: bkg2,
+    alt: 'work-5',
+    col: 12,
+  },
+  {
+    title: 'Title Project',
+    label: 'See project description',
+    url: '/',
+    image: bkg3,
+    alt: 'work-6',
+    col: 12,
+  },
+];
+
 export const Work = () => {
+  const [selectedItem, setSelectedItem] = useState<number | null>(null);
+
+  const handleOnClick = (index: number) => {
+    setSelectedItem(index);
+  };
+
+  const handleLeave = () => {
+    setSelectedItem(null);
+  };
+
   return (
     <Component id="work">
       <Row>
-        <Col>
-          <img src={bkg1} alt="background-1" width="100%" height="100%" />
-        </Col>
-      </Row>
-
-      <Row>
-        <Col span={24} md={12}>
-          <img src={bkg2} alt="background-2" width="100%" height="100%" />
-        </Col>
-
-        <Col span={24} md={12}>
-          <img src={bkg3} alt="background-3" width="100%" height="100%" />
-        </Col>
-      </Row>
-
-      <Row>
-        <Col>
-          <img src={bkg1} alt="background-1" width="100%" height="100%" />
-        </Col>
-      </Row>
-
-      <Row>
-        <Col span={24} md={12}>
-          <img src={bkg2} alt="background-2" width="100%" height="100%" />
-        </Col>
-
-        <Col span={24} md={12}>
-          <img src={bkg3} alt="background-3" width="100%" height="100%" />
-        </Col>
+        {listWorks.map((work, index) => {
+          return (
+            <Col key={work.alt + 1} span={work.col}>
+              <WrapperImage onClick={() => handleOnClick(index)} onMouseLeave={handleLeave}>
+                <Hovered hovered={selectedItem === index}>
+                  <Title>{work.title}</Title>
+                  <LinkWork to={work.url}>{work.label}</LinkWork>
+                </Hovered>
+                <Image src={work.image} alt={work.alt} width="100%" height="100%" />
+              </WrapperImage>
+            </Col>
+          );
+        })}
       </Row>
     </Component>
   );
--- src/pages/home/components/work/styles.ts
+++ src/pages/home/components/work/styles.ts
@@ -1,5 +1,62 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Component = styled.div`
   width: 100%;
+`;
+
+export const WrapperImage = styled.div`
+  position: relative;
+  cursor: pointer;
+`;
+
+export const Hovered = styled.div<{ hovered: boolean }>`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 87, 255, 0.75);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  position: absolute;
+  opacity: 0;
+  transition: all ease-in-out 0.5s;
+
+  ${({ hovered }) => hovered && 'opacity: 1;'}
+`;
+
+export const Image = styled.img``;
+
+export const Title = styled.h3`
+  color: white;
+  font-size: 32px;
+  font-weight: normal;
+  margin: 0px 0px 10px 0px;
+`;
+
+export const LinkWork = styled(Link)`
+  color: white;
+  font-size: 18px;
+  font-weight: 300;
+  position: relative;
+
+  &:hover {
+    color: white;
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: -5px;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 0;
+    height: 1px;
+    background-color: white;
+    transition: width 0.3s ease-out;
+  }
+
+  &:hover::after {
+    width: 100%;
+  }
 `;
